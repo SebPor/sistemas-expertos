597235cadd09c4e1ef20178caa2101e91b76694e
Sebas <sebaspxabia@gmail.com>
2023-10-20 20:07:48 +0200

    Empezado el diagrama

---
+++ Tipos de lenguajes.mm
@@ -0,0 +1,95 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Tipos de lenguajes" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1697736576397" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1695550830000"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Declarativos" POSITION="bottom_or_right" ID="ID_977658215" CREATED="1697736569480" MODIFIED="1697737151213" HGAP_QUANTITY="-293.20001 pt" VSHIFT_QUANTITY="-10.2 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Funcional" ID="ID_1069408546" CREATED="1697736795371" MODIFIED="1697737132823" HGAP_QUANTITY="-109.6 pt" VSHIFT_QUANTITY="-3.6 pt"/>
+<node TEXT="Lógica" ID="ID_1965241883" CREATED="1697736801155" MODIFIED="1697737128503" HGAP_QUANTITY="-98.2 pt"/>
+</node>
+<node TEXT="Imperativos" POSITION="top_or_left" ID="ID_1732691303" CREATED="1697736572675" MODIFIED="1697737155179" HGAP_QUANTITY="-281.80001 pt" VSHIFT_QUANTITY="-6 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="Estructurada" ID="ID_1967181852" CREATED="1697736663516" MODIFIED="1697736722799" HGAP_QUANTITY="-128.2 pt" VSHIFT_QUANTITY="-1.8 pt">
+<node TEXT="C" ID="ID_1709102913" CREATED="1697736813294" MODIFIED="1697736921919" HGAP_QUANTITY="-94 pt" VSHIFT_QUANTITY="-0.6 pt"/>
+<node TEXT="ALGOL" ID="ID_472395752" CREATED="1697736817471" MODIFIED="1697737056464" HGAP_QUANTITY="-109 pt" VSHIFT_QUANTITY="5.4 pt"/>
+</node>
+<node TEXT="Orientada a Objetos" ID="ID_1682328348" CREATED="1697736701200" MODIFIED="1697736719515" HGAP_QUANTITY="-155.2 pt" VSHIFT_QUANTITY="-9 pt">
+<node TEXT="Java" ID="ID_1326100796" CREATED="1697736894858" MODIFIED="1697736909078" HGAP_QUANTITY="-126.4 pt"/>
+<node TEXT="C#" ID="ID_1764851158" CREATED="1697736900811" MODIFIED="1697736906127" HGAP_QUANTITY="-124 pt"/>
+</node>
+<node TEXT="Procedimental" ID="ID_1631536302" CREATED="1697736730048" MODIFIED="1697736743066" HGAP_QUANTITY="-133 pt">
+<node TEXT="Pascal" ID="ID_1569484307" CREATED="1697737061506" MODIFIED="1697737070226" HGAP_QUANTITY="-109.6 pt" VSHIFT_QUANTITY="-11.4 pt"/>
+<node TEXT="Fortran" ID="ID_1717663031" CREATED="1697737074911" MODIFIED="1697737094183" HGAP_QUANTITY="-114.4 pt"/>
+</node>
+<node TEXT="Modular" ID="ID_1361790688" CREATED="1697736763698" MODIFIED="1697736778081" HGAP_QUANTITY="-118 pt" VSHIFT_QUANTITY="12 pt"/>
+<node TEXT="Por eventos" ID="ID_1336788538" CREATED="1697736783552" MODIFIED="1697736792428" HGAP_QUANTITY="-130.6 pt" VSHIFT_QUANTITY="-43.2 pt"/>
+</node>
+</node>
+</map>
